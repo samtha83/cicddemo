c1d39cfd91a660a335154b00f525969075ad34ca
samtha83 <samtha.learn@gmail.com>
2025-04-21 12:24:29 -0400

    new change to app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from my CI/CD powered Flask app! V1 sam'
+    return 'Hello from my CI/CD powered Flask app! V1 samtha'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
