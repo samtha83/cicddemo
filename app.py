acfbf3b97b102f73a7203e80e0e98c3170d66eec
samtha83 <samtha.learn@gmail.com>
2025-04-21 12:28:49 -0400

    new change to app

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
     return 'Hello from my CI/CD powered Flask app! V1 samtha'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0')
